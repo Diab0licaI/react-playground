a83dbb53c092d6c7c397c47c0de1f530bcdd532b
Diab0lical <work.dipukumar@gmail.com>
2026-01-14 23:59:02 +0530

    Add CRA and Vite React projects

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+   <h1>dipu kumar rajak</h1>
+  );
+}
+
+export default App;
---
+++ vite-project/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  
+
+  return (
+    <h1>chai aur react with vite | dipu kumar rajak</h1>
+  )
+}
+
+export default App
