f83029e7dbe038291096ef9c645785f7927d7ea3
Diab0lical <work.dipukumar@gmail.com>
2026-01-18 22:14:08 +0530

    more_project

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -31,10 +31,13 @@ function App() {
      <button
      onClick={addValue}
      >Add value {counter} </button>
+
      <br/>
+     
      <button
      onClick={removeValue}
      >remove value {counter} </button>
+
      <p>footer: {counter} </p>
     </>
   )
